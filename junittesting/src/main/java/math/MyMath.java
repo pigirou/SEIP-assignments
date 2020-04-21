4e37ea3198abb2b771a3b7ca0e0bca2cdee9996d
pigi.rou@gmail.com <pigi.rou@gmail.com>
2020-04-21 14:46:04 +0300

    Add javadoc comments to MyMath class

--- junittesting/src/main/java/math/MyMath.java
+++ junittesting/src/main/java/math/MyMath.java
@@ -1,11 +1,20 @@
 package math;
 
+/**
+ * A class that calculates mathematical operations in order to demonstrate Unit
+ * Testing.
+ * 
+ * @author pigirou (pigi.rou@gmail.com)
+ */
+
 public class MyMath {
-	
-	/*DON'T FORGET TO ADD JAVADOC COMMENTS*/
+	/**
+	 * Receives an integer number as input and calculates the factorial. Throws
+	 * IllegalArgumentException when the number does not lie between 0 and 12
+	 */
 
 	public static int factorial(int n) {
-		if (n<0 || n>12) {
+		if (n < 0 || n > 12) {
 			throw new IllegalArgumentException("The number should lie between 0 and 12");
 		}
 		int f = 1;
