c0f22405635b2efc1bde4d56ab8537ea06b1cedc
pigi.rou@gmail.com <pigi.rou@gmail.com>
2020-04-21 19:09:02 +0300

    Add two seperate if statements and seperate messages when exception occurs

--- junittesting/src/main/java/math/MyMath.java
+++ junittesting/src/main/java/math/MyMath.java
@@ -14,8 +14,11 @@ public class MyMath {
 	 */
 
 	public static int factorial(int n) {
-		if (n < 0 || n > 12) {
-			throw new IllegalArgumentException("The number should lie between 0 and 12");
+		if (n < 0 ) {
+			throw new IllegalArgumentException("The number should be above 0");
+		}
+		if (n>12) {
+			throw new IllegalArgumentException("The number should be below 12");
 		}
 		int f = 1;
 		for (int i = 2; i <= n; i++) {
