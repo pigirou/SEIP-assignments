3dcb56ab077f033af28069e6ff43898597c9a332
pigi.rou@gmail.com <pigi.rou@gmail.com>
2020-04-22 14:41:08 +0300

    Add test cases for MyMath isPrime method

--- junittesting/src/test/java/org/junittesting/MyMathTest.java
+++ junittesting/src/test/java/org/junittesting/MyMathTest.java
@@ -1,5 +1,6 @@
 package org.junittesting;
 
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
@@ -24,4 +25,17 @@ public class MyMathTest {
 		a.factorial(13);
 	}
 
+	@Test 
+	public void test_isprime_nonvalid() {
+		thrown.expectMessage("The number given should be above or equal to 2");
+		a.isPrime(0);
+	}
+	
+	@Test 
+	public void test_isprime_valid() {
+		int expected = 3;
+		Assert.assertEquals(expected, a.isPrime(3));
+	}
+	
+	
 }
