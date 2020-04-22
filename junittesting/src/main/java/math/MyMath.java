5dcedee8cbfe3b429dec0c8e3b4ddfc228ec0805
pigi.rou@gmail.com <pigi.rou@gmail.com>
2020-04-22 14:38:11 +0300

    Add isPrime method to MyMath

--- junittesting/src/main/java/math/MyMath.java
+++ junittesting/src/main/java/math/MyMath.java
@@ -14,10 +14,10 @@ public class MyMath {
 	 */
 
 	public static int factorial(int n) {
-		if (n < 0 ) {
+		if (n < 0) {
 			throw new IllegalArgumentException("The number should be above 0");
 		}
-		if (n>12) {
+		if (n > 12) {
 			throw new IllegalArgumentException("The number should be below 12");
 		}
 		int f = 1;
@@ -25,6 +25,24 @@ public class MyMath {
 			f = f * i;
 		}
 		return f;
+	}
+
+	public boolean isPrime(int n) {
+		boolean flag = false;
+		if (n < 2) {
+			throw new IllegalArgumentException("The number given should be above or equal to 2");
+		}
+		else {
+			
+		}
+
+		for (int i = 2; i <= n / 2; ++i) {
+			if (n % i == 0) {
+				flag = true;
+				break;
+			}
+		}
+		return flag;
 	}
 
 }
