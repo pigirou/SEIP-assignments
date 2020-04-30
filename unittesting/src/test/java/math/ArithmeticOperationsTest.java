002088673ad475bde9a18b7b1106f9340c1b2d40
pigi.rou@gmail.com <pigi.rou@gmail.com>
2020-04-30 23:01:44 +0300

    Add forgotten import for Assert

--- unittesting/src/test/java/math/ArithmeticOperationsTest.java
+++ unittesting/src/test/java/math/ArithmeticOperationsTest.java
@@ -4,6 +4,7 @@ import math.ArithmeticOperations;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.Rule;
+import org.junit.Assert;
 
 
 public class ArithmeticOperationsTest {
